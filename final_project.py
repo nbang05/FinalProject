40dbb15b1ae68fdd9627d6bcf72dff7f1dd44df8
Naomi Bang <naomisajin@gmail.com>
2023-12-20 05:55:45 -0500

    webscrape and create csv

--- final_project.py
+++ final_project.py
@@ -12,8 +12,50 @@
 # store data in dictionary (5.15-9)
 # JSON is represented as dictionary before stringified
 
+# billboard
+
+# this program scrapes Wikipedia's "List of Billboard Hot 100 number ones of 2023"
+# and compiles the list of top songs this year into a dataframe
+
+from bs4 import BeautifulSoup
+import requests
 import pandas as pd
+import re
 import random
+
+# using beautifulsoup to parse website content
+# checklist item 10.1-2
+url = "https://en.wikipedia.org/wiki/List_of_Billboard_Hot_100_number_ones_of_2023"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# find track names
+table = soup.find('table', class_ = 'wikitable sortable plainrowheaders') # isolate tracklist table
+body = table.find('tbody') # going down in tree
+columnHeaders = body.find('tr') # finds row of column headers (no., song, artist, etc.)
+indivColumn = columnHeaders.find_all('th')[2] # isolates song column only
+titles = [title.text.strip() for title in indivColumn] # formats title into text, strip() gets rid of extra indent
+
+# using pandas to create dataframe
+df = pd.DataFrame(columns = titles) 
+
+# going down in tree
+columnData = body.find_all('tr')[1:]
+
+# finds song in specified column from each row
+for row in columnData: # iterates through each row in the whole table, appends song title each time it loops through 
+    songTitle = row.find('a', title=re.compile(r'.*')) # finds song title
+    
+    # convert titles to text and append to dataframe
+    if songTitle:
+        indivRowData = songTitle.text # converts each title to text
+        df.loc[len(df.index)] = [indivRowData] # checks how many columns in dataframe each loop and puts data in next row
+
+print(df) # print dataframe
+
+# write dataframe into csv
+# checklist item 8.4
+df.to_csv(r'/Users/naobae/Library/FinalProject/Billboard100.csv', index = False)
 
 # import csv file
 # checklist item 8.2
@@ -58,4 +100,3 @@ print(songGenre)
 # can you make pandas data into dictionary?
 # need to learn licensing
 # once user has curated a playlist, use numpy to calculate how many songs each time add?
-    
